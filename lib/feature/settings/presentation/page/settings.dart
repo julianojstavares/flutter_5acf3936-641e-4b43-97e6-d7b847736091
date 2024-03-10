3641798eb8e69c5ae2c0c6a52461fd27023903ff
Juliano J. S. Tavares <oi@julianotavares.dev>
2024-03-10 16:52:39 -0300

    Extracting SettingsPage AppBar

--- lib/feature/settings/presentation/page/settings.dart
+++ lib/feature/settings/presentation/page/settings.dart
@@ -1,8 +1,8 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/header_widget.dart';
+import '../widgets/settings_page_appbar.dart';
 
 @RoutePage()
 class SettingsPage extends StatelessWidget {
@@ -10,26 +10,9 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 40,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            size: 15,
-          ),
-        ),
-        titleSpacing: 0,
-        title: const AutoSizeText(
-          "Configurações",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: const Column(
+    return const Scaffold(
+      appBar: SettingsPageAppBar(),
+      body: Column(
         children: [
           HeaderWidget(),
         ],
---
+++ lib/feature/settings/presentation/widgets/settings_page_appbar.dart
@@ -0,0 +1,34 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPageAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const SettingsPageAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leadingWidth: 40,
+      leading: IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.arrow_back_ios_new,
+          size: 15,
+        ),
+      ),
+      titleSpacing: 0,
+      title: const AutoSizeText(
+        "Configurações",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
