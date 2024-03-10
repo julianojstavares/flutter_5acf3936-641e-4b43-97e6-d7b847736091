4b67111641ddcaafd682bf52e2b5843e43e24899
Juliano J. S. Tavares <oi@julianotavares.dev>
2024-03-10 16:39:10 -0300

    Removing unused imports

--- lib/feature/settings/presentation/page/settings.dart
+++ lib/feature/settings/presentation/page/settings.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 @RoutePage()
 class SettingsPage extends StatelessWidget {
