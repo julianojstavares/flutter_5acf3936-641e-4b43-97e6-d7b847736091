a93ca10c5e5d336614a7f8af07f782009e99aafe
Juliano J. S. Tavares <oi@julianotavares.dev>
2024-03-11 15:44:49 -0300

    Adding period popup

--- lib/feature/settings/presentation/page/settings.dart
+++ lib/feature/settings/presentation/page/settings.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/feature/settings/presentation/widgets/period_popup.dart';
 
+import '../cubit/dialog_cubit.dart';
 import '../widgets/content_widget.dart';
 import '../widgets/footer_widget.dart';
 import '../widgets/header_widget.dart';
@@ -12,24 +15,38 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: SettingsPageAppBar(),
+    return Scaffold(
+      appBar: const SettingsPageAppBar(),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              HeaderWidget(),
-              Divider(),
-              ContentWidget(),
-              SizedBox(height: 50),
-              FooterWidget(),
-            ],
+          padding: const EdgeInsets.all(20),
+          child: BlocProvider(
+            create: (context) => DialogCubit(),
+            child: BlocListener<DialogCubit, bool>(
+              listener: (context, isOpen) {
+                final dialogCubit = context.read<DialogCubit>();
+                if (isOpen) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => PeriodPopUp(
+                      dialogCubit: dialogCubit,
+                    ),
+                  );
+                }
+              },
+              child: const Column(
+                children: [
+                  HeaderWidget(),
+                  Divider(),
+                  ContentWidget(),
+                  SizedBox(height: 50),
+                  FooterWidget(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
