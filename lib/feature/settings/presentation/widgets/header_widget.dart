055744093208b9e8339c5241220abe9268577ebf
Juliano J. S. Tavares <oi@julianotavares.dev>
2024-03-12 18:50:16 -0300

    Working with image picker

--- lib/feature/settings/presentation/widgets/header_widget.dart
+++ lib/feature/settings/presentation/widgets/header_widget.dart
@@ -1,10 +1,23 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
-class HeaderWidget extends StatelessWidget {
+class HeaderWidget extends StatefulWidget {
   const HeaderWidget({
     super.key,
   });
+
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  final ImagePicker imgPicker = ImagePicker();
+  XFile? imgLocal;
 
   @override
   Widget build(BuildContext context) {
@@ -37,25 +50,38 @@ class HeaderWidget extends StatelessWidget {
           ),
           child: Row(
             children: [
-              const CircleAvatar(
-                foregroundImage: AssetImage("assets/images/profile.png"),
+              CircleAvatar(
+                foregroundImage: imgLocal != null
+                    ? FileImage(File(imgLocal!.path)) as ImageProvider<Object>?
+                    : const AssetImage("assets/images/profile.png"),
                 radius: 25,
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () async {
+                  imgLocal = await imgPicker.pickImage(
+                    source: ImageSource.gallery,
+                  );
+                  Directory appDocDir =
+                      await getApplicationDocumentsDirectory();
+                  String appDocPath = appDocDir.path;
+
+                  String novoCaminho = '$appDocPath/${imgLocal!.name}';
+
+                  File imagemSelecionada = File(imgLocal!.path);
+                  await imagemSelecionada.copy(novoCaminho);
+
+                  log(imgLocal?.path ?? "no image");
+                  log(novoCaminho);
+
+                  setState(() {});
+                },
                 style: const ButtonStyle(
-                  padding: MaterialStatePropertyAll(EdgeInsets.zero),
+                  foregroundColor: MaterialStatePropertyAll(Colors.black),
+                  overlayColor: MaterialStatePropertyAll(Colors.blue),
                 ),
-                child: TextButton(
-                  onPressed: () {},
-                  style: const ButtonStyle(
-                    foregroundColor: MaterialStatePropertyAll(Colors.black),
-                    overlayColor: MaterialStatePropertyAll(Colors.blue),
-                  ),
-                  child: const AutoSizeText(
-                    "Editar Foto",
-                    style: TextStyle(fontSize: 11),
-                  ),
+                child: const AutoSizeText(
+                  "Editar Foto",
+                  style: TextStyle(fontSize: 11),
                 ),
               ),
             ],
