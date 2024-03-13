fc8f9ed55c1de7cff7c345c5b037649c2ce3c4d4
Juliano J. S. Tavares <oi@julianotavares.dev>
2024-03-13 14:59:37 -0300

    Setting up the pt-br language

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 import 'core/routes/app_router.dart';
 
@@ -13,6 +18,15 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   final _appRouter = AppRouter();
 
+  Future<void> setupIntl() async {
+    try {
+      Intl.defaultLocale = 'pt_BR';
+      await initializeDateFormatting('pt_BR');
+    } catch (e) {
+      log('Erro na inicialização do formato de data: $e');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -22,12 +36,24 @@ class _AppState extends State<App> {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
+
+    setupIntl().then((_) {
+      setState(() {});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: _appRouter.config(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt', "BR"),
+      ],
     );
   }
 }
